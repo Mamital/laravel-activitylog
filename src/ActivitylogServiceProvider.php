2f00d3a18158cfe0d29064a3b6266a0b35784932
Mohammad Sadigh <mohammadsadigh94@gmail.com>
2026-01-04 21:47:35 +0330

    register added migration

--- src/ActivitylogServiceProvider.php
+++ src/ActivitylogServiceProvider.php
@@ -21,6 +21,7 @@ class ActivitylogServiceProvider extends PackageServiceProvider
             'create_activity_log_table',
             'add_event_column_to_activity_log_table',
             'add_batch_uuid_column_to_activity_log_table',
+            'add_before_after_column_to_activity_log_table',
         ])
         ->hasCommand(CleanActivitylogCommand::class);
     }
